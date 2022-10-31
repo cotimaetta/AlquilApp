c455060e37c87478478b17b6ce617ff0dd39bb3a
Calvo <matias.calvo.2000@gmail.com>
2022-10-31 17:33:11 -0300

    Agregue auto a la bd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_031241) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_200621) do
+  create_table "autos", force: :cascade do |t|
+    t.string "patente"
+    t.string "marca"
+    t.string "modelo"
+    t.integer "cant_puertas"
+    t.text "descripcion"
+    t.float "cant_combustible"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -31,4 +42,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_031241) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
