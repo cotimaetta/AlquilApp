fd62c2bffca1f9036f48d2a8630b9e33e2f3288a
Valentina <valenmaranzanaa@gmail.com>
2022-11-02 13:10:21 -0300

    validaciones user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nombre, :apellido, :email, presence:true
+  #validates :fecha_nac, :fecha_ven, presence:true
   validates :dni, numericality: { only_integer: true } , uniqueness: true
   validate :menor
   validate :vencimiento
@@ -15,14 +16,26 @@ class User < ApplicationRecord
   enum rol: [:admin, :supervisor, :usuario]
  
   def menor
-    if(fecha_nac > 21.years.ago)
-      errors.add(:base ,message:"no tenes edad para manejar")
+    if(usuario?)
+      if fecha_nac != nil
+        if fecha_nac > 21.years.ago
+          errors.add(:base ,message:"no tenes edad para manejar")
+        end
+      else
+          errors.add(:base ,message:"Debes ingresar tu fecha de nacimiento")
+      end
     end
   end
 
   def vencimiento
-    if(Date.today > fecha_ven)
-      errors.add(:base ,message:"Tenes tu licencia de conducir vencida")
+    if(usuario?)
+      if fecha_nac != nil
+        if Date.today > fecha_ven
+          errors.add(:base ,message:"Tenes tu licencia de conducir vencida")
+        end
+      else
+          errors.add(:base ,message:"Debes ingresar la fecha de vencimiento de tu licencia de conducir")
+      end
     end
   end
 
