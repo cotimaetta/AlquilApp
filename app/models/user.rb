e015eb643e142397cfe3c1154e34513dd54e5a12
Valentina <valenmaranzanaa@gmail.com>
2022-11-01 21:34:30 -0300

    actualice modelo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,22 @@ class User < ApplicationRecord
   # has_one_attached :cover_picture
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nombre, :apellido, presence:true
-  validates :dni, numericality: { only_integer: true }
+
+  validates :nombre, :apellido, :email, presence:true
+  validates :dni, numericality: { only_integer: true } , uniqueness: true
   validate :menor
+  validate :vencimiento
  
   def menor
     if(fecha_nac > 21.years.ago)
       errors.add(:base ,message:"no tenes edad para manejar")
     end
   end
+
+  def vencimiento
+    if(Date.today > fecha_ven)
+      errors.add(:base ,message:"Tenes tu licencia de conducir vencida")
+    end
+  end
+
 end
