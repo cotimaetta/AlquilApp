54fb48b92af72b8ea6b4047216305d5003c95717
cotimaetta <cotimaetta@gmail.com>
2022-11-05 21:21:16 -0300

    probando

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_132959) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_05_232246) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_132959) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -49,10 +49,26 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_132959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "pos"
+    t.boolean "alquilado"
+    t.boolean "en_servicio"
+  end
+
+  create_table "foto_usuarios", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "fotousers", force: :cascade do |t|
     t.integer "id_user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "historial_de_uso", force: :cascade do |t|
+    t.datetime "inicio_alquiler"
+    t.datetime "fin_alquiler"
+    t.float "tiempo_alquiler"
+    t.float "monto"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
