df0b528699652f12fd4e702cd4e4796ae2a949a4
Calvo <matias.calvo.2000@gmail.com>
2022-11-07 21:25:52 -0300

    mapa a los autos en el show

--- app/controllers/autos_controller.rb
+++ app/controllers/autos_controller.rb
@@ -43,7 +43,7 @@ class AutosController < ApplicationController
   private
 
   def auto_params
-    params.require(:auto).permit(:patente, :marca, :descripcion, :cant_puertas, :cant_combustible, :location_point, :foto)
+    params.require(:auto).permit(:patente, :marca, :descripcion, :cant_puertas, :cant_combustible, :location_point_x, :location_point_y, :foto)
   end
 
 end
