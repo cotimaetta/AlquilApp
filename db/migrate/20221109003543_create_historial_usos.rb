d1fcceaa4b6b83abe067bf8d012379e5d4bd3911
Calvo <matias.calvo.2000@gmail.com>
2022-11-09 03:56:59 -0300

    Horas extras

--- db/migrate/20221109003543_create_historial_usos.rb
+++ db/migrate/20221109003543_create_historial_usos.rb
@@ -5,7 +5,7 @@ class CreateHistorialUsos < ActiveRecord::Migration[7.0]
       t.datetime :fechaInicio
       t.datetime :fechaFinal
       t.integer :cantHoras
-      t.integer :cantHoras
+      t.integer :horasExtra
       
       t.references :user, null: false, foreign_key: true
       t.references :auto, null: false, foreign_key: true
