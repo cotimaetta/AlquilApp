ec5af424b88e109801168476db091a32accd36ed
Valentina <valenmaranzanaa@gmail.com>
2022-11-14 20:10:17 -0300

    modifique agregar auto

--- app/models/auto.rb
+++ app/models/auto.rb
@@ -3,7 +3,7 @@ class Auto < ApplicationRecord
     has_one_attached :fotodocumentacion
     has_many :historialusos
     
-
-    validates :patente, :marca, :modelo, :cant_puertas, :cant_combustible, :foto, :fotodocumentacion,   presence:true
+    validates :patente, presence:true, uniqueness: true
+    validates :marca, :modelo, :cant_puertas, :cant_combustible, :foto, :fotodocumentacion, presence:true
     
 end
